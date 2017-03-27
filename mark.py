3e57ab5294b2c3bf6274d72453d7904c9aa8ce4b
Arkarin <943710803@qq.com>
2017-03-27 22:35:48 +0800

    Create mark.py

---
+++ mark.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import os
+
+srcdir = './unlabel/'
+dstdir = './label/'
+
+flist = os.listdir(srcdir)
+fcont = len(flist)
+
+stidx = len(os.listdir(dstdir))
+
+for fidx in range(fcont):
+    fulldir = srcdir + flist[fidx]
+    im = Image.open(fulldir)
+    plt.imshow(im)
+    plt.show()
+    lbstr = raw_input("Input the label of image above (input 'EXIT' to end):")
+    if lbstr == 'exit' or lbstr == 'EXIT':
+        print "exit..."
+        break
+    lbstr = str(lbstr)
+    stridx = str(fidx + stidx).zfill(4)
+    newdir = dstdir + stridx + '_' + lbstr + '.jpg'
+    
+    im.save(newdir)
+    os.remove(fulldir)
+    
+print "All the files have been labeled!"
